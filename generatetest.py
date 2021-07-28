b86ae4fa8aada8b8e250adb3724015b3ca9febc0
vickyandreas <vicky.andreas@alpabit.com>
2021-07-29 00:46:57 +0700

    add comment for more information and beautufy code

--- generatetest.py
+++ generatetest.py
@@ -5,110 +5,107 @@ from typing import Dict, List, NamedTuple, Optional, Tuple
 import mysql.connector as mysql
 
 db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "",
+    host="localhost",
+    user="root",
+    passwd="root",
     database="ta2",
 )
 
 cursor = db.cursor()
-ta="select * from app_ta_matkul_kelas"
+ta = "select * from app_ta_matkul_kelas"
 cursor.execute(ta)
 tas = cursor.fetchall()
 
 
-akun="select * from app_akun"
+akun = "select * from app_akun"
 cursor.execute(akun)
 akuns = cursor.fetchall()
 
 
-dosen="SELECT dms.no_dosen_id,dms.no_kelas_id,dms.no_matkul_id,matkul.sks,matkul.kategori_id FROM app_dosen_matkul_kelas dms INNER JOIN app_matkul matkul ON dms.no_matkul_id=matkul.id ORDER BY kategori_id ASC limit 40"
-cursor.execute(dosen)
-dosens = cursor.fetchall()
+pmk = "SELECT * from pmk limit 20"
+cursor.execute(pmk)
+pmks = cursor.fetchall()
 
 
-hari="select * from app_hari"
+hari = "select * from app_hari"
 cursor.execute(hari)
 haris = cursor.fetchall()
 
-kelas="select * from app_kelas"
+kelas = "select * from app_kelas"
 cursor.execute(kelas)
 kelass = cursor.fetchall()
 
-matkul="select * from app_matkul"
+matkul = "select * from app_matkul"
 cursor.execute(matkul)
 matkuls = cursor.fetchall()
 
-kategori="select * from app_kategori"
+kategori = "select * from app_kategori"
 cursor.execute(kategori)
 kategoris = cursor.fetchall()
 
-sesi="select * from app_sesi"
+sesi = "select * from app_sesi"
 cursor.execute(sesi)
 sesis = cursor.fetchall()
 
 # [print(list(list(row)))for row in dosens]
 
-Grid = List[List[Tuple]]
+Grid = List[List[List[Tuple]]]
+
 
 class GridSchedule(NamedTuple):
     row: int
     column: int
 
-def generate_grid(rows: int, columns: int)->Grid:
-       return [[[row for row in dosens] for j in range(columns)] for i in range(rows)]
 
-def display_grid(grid: Grid)->None:
+def generate_grid(rows: int, columns: int) -> Grid:
+    return [[[] for j in range(columns)] for i in range(rows)]
+
+
+def display_grid(grid: Grid) -> None:
     for row in grid:
         print("".join(row))
 
- 
-   
+
 def generate_domain(grid: Grid) -> List[List[GridSchedule]]:
     domain: List[List[GridSchedule]] = []
-    
+
     height: int = len(grid)
     width: int = len(grid[0])
     for row in range(height):
         for col in range(width):
             domain.append([GridSchedule(row, col)])
-    # print(domain)
     return domain
 
 
-class GenerateConstraint(Constraint[Tuple, List[GridSchedule]]):
-    def __init__(self, dms: List[Tuple]) -> None:
-        super().__init__(dms)
-        self.words: List[Tuple] = dms
+class GenerateConstraint(Constraint[str, List[GridSchedule]]):
+    def __init__(self, pmks: List[Tuple]) -> None:
+        super().__init__(pmks)
+        self.pmks: List[Tuple] = pmks
 
-    def satisfied(self, assignment: Dict[Tuple, List[GridSchedule]]) -> bool:
+    def satisfied(self, assignment: Dict[Tuple, List[GridSchedule]]) -> None:
         # if there are any duplicates grid locations then there is an overlap
-        # print(assignment)
+        print(assignment)
         # all_locations = [locs for values in assignment.values() for locs in  values]
         # return len(set(all_locations)) == len(all_locations)
         return True
 
-    
-
-
- 
 
 if __name__ == "__main__":
     jumlah_sesi = len(sesis)
     jumlah_hari = len(haris)
-    
-    grid: Grid=generate_grid(jumlah_hari, jumlah_sesi)
-    dms : List[Tuple] =  [row for row in dosens]
+
+    grid: Grid = generate_grid(jumlah_hari, jumlah_sesi)
+    pmk: List[Tuple] = [row for row in pmks]
     location: Dict[Tuple, List[List[GridSchedule]]] = {}
-    for items in dms:
-        location[items]= generate_domain(grid)
-    # for dms in dms:
-    #         location[dms] = generate_domain(dms, grid)
-    csp: CSP[Tuple, List[GridSchedule]] = CSP(dms, location)
-    csp.add_constraint(GenerateConstraint(dms))
 
+    for items in pmk:
+        location[items] = generate_domain(grid)
 
-    # solution: Optional[Dict[Tuple, List[GridSchedule]]] = csp.backtracking_search()
+    csp = CSP(pmk, location)
+    csp.add_constraint(GenerateConstraint(pmk))
+
+    solution: Optional[Dict[Tuple, List[GridSchedule]]
+                       ] = csp.backtracking_search()
     # if solution is None:
     #     print("No solution found!")
     # else:
@@ -117,5 +114,3 @@ if __name__ == "__main__":
     #             (row, col) = (grid_locations[index].row, grid_locations[index].column)
     #             grid[row][col] = letter
     #     display_grid(grid)
-
-  
--- word_search.py
+++ word_search.py
@@ -34,7 +34,8 @@ def generate_domain(word: str, grid: Grid) -> List[List[GridLocation]]:
                 # left to right
                 domain.append([GridLocation(row, c) for c in columns])
                 if row + length <= height:
-                    domain.append([GridLocation(r, col + (r - row)) for r in rows])
+                    domain.append([GridLocation(r, col + (r - row))
+                                  for r in rows])
 
             if row + length <= height:
                 # top to bottom
@@ -44,7 +45,8 @@ def generate_domain(word: str, grid: Grid) -> List[List[GridLocation]]:
                 # print([GridLocation(r, col) for r in rows])
                 # diagonal towards bottom left
                 if col - length >= 0:
-                    domain.append([GridLocation(r, col - (r - row)) for r in rows])
+                    domain.append([GridLocation(r, col - (r - row))
+                                  for r in rows])
                     # print("diagonal towards bottom left")
                     # print([GridLocation(r, col - (r - row)) for r in rows])
     return domain
@@ -56,10 +58,10 @@ class WordSearchConstraint(Constraint[str, List[GridLocation]]):
         self.words: List[str] = words
 
     def satisfied(self, assignment: Dict[str, List[GridLocation]]) -> bool:
-        all_locations = [locs for values in assignment.values() for locs in  values]
+        all_locations = [locs for values in assignment.values()
+                         for locs in values]
         return len(set(all_locations)) == len(all_locations)
 
-    
 
 if __name__ == "__main__":
     grid: Grid = generate_grid(5, 5)
@@ -70,16 +72,18 @@ if __name__ == "__main__":
         locations[word] = generate_domain(word, grid)
     csp: CSP[str, List[GridLocation]] = CSP(words, locations)
     csp.add_constraint(WordSearchConstraint(words))
-    solution: Optional[Dict[str, List[GridLocation]]] = csp.backtracking_search()
-    if solution is None:
-        print("No solution found!")
-    else:
-        for word, grid_locations in solution.items():
-            # random reverse half the time
-            if choice([True, False]):
-                grid_locations.reverse()
-            for index, letter in enumerate(word):
-                (row, col) = (grid_locations[index].row, grid_locations[index].column)
-                grid[row][col] = letter
-        # print("\n")
-        display_grid(grid)
+    solution: Optional[Dict[str, List[GridLocation]]
+                       ] = csp.backtracking_search()
+    # if solution is None:
+    #     print("No solution found!")
+    # else:
+    #     for word, grid_locations in solution.items():
+    #         # random reverse half the time
+    #         if choice([True, False]):
+    #             grid_locations.reverse()
+    #         for index, letter in enumerate(word):
+    #             (row, col) = (
+    #                 grid_locations[index].row, grid_locations[index].column)
+    #             grid[row][col] = letter
+    #     # print("\n")
+    #     display_grid(grid)
